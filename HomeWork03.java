6d512be24dd66f7959903803bf88ea2c46fad9d9
GarfieldWang3658 <116738842+GarfieldWang3658@users.noreply.github.com>
2023-09-05 22:06:45 +0900

    Add files via upload

--- HomeWork03.java
+++ HomeWork03.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 public class HomeWork03{
 	public static void main(String[] args) {
-		Book b=new Book();
+
 		Scanner myScanner = new Scanner(System.in);
+		System.out.println("请输入书名");
+		String name = myScanner.next();
 		System.out.println("请输入书本价格");
 		double price = myScanner.nextDouble();
-		System.out.println("更改后的书本价格为" + b.updatePrice(price));
+		Book b= new Book(name,price);
+		b.info();
+        b.updatePrice(b.price);
+		b.info();
 	}
 }
 
 class Book{
-	public double updatePrice(double price){
-		if (price>150) {
-			price=150;
-		}else if (price>100&&price<150) {
-			price=100;
+	String name;
+	double price;
+	public Book(String name,double price){
+		this.name=name;
+		this.price=price;
+	}
+
+	public void updatePrice(double price){
+		//如果方法中没有price局部变量，this.price 等价price
+		if (this.price>150) {
+			this.price=150;
+		}else if (this.price>100 && this.price<=150) {
+			this.price=100;
 		}
-		return price;
+	}
+	//显示书籍的情况。
+
+	public void info(){
+		System.out.println("书籍的名称为"+this.name+"书籍的价格为"+this.price);
 	}
 }
